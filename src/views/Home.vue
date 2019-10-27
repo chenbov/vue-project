b43a592ff36302a3252e48f8602c1f6f315f60f8
chenbo <chbest1028@163.com>
2019-10-27 15:02:24 +0800

    添加登录页

---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="This is an home page" />
+    <div ref="www">{{title}}</div>
+    <button @click="handle">测试</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  data () {
+    return {
+      route: this.$route,
+      age: 27
+    }
+  },
+  created () {
+    console.log(this.$route)
+    console.log(this.$router)
+  },
+  methods: {
+    handle () {
+      this.title = '123'
+      console.log(this.title) // 123
+      console.log(this.$refs.www.innerHtml) // undefined
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  watch: {
+
+  }
+}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,62 @@
+<template>
+  <div :class="{'login':setColor}">
+    <h1>vue项目登录页</h1>
+    <div>
+      <label for="">权限类型：</label>
+      <select v-model="selected">
+        <option :value="index" v-for="(item,index) in user" :key="index">{{item}}</option>
+      </select>
+    </div>
+    <button @click="changeRouter">登录</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      user: ['管理员', '操作员'],
+      setColor: true,
+      selected: '0'
+    }
+  },
+  methods: {
+    changeRouter () {
+      if (this.selected === '0') {
+        this.$router.push('/home')
+      } else {
+        this.$router.push('/about')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped="scoped" lang="less">
+  .login {
+    width: 300px;
+    height: 250px;
+    margin: 100px auto;
+    border: 1px solid #ccc;
+    font-size: 14px;
+
+    div {
+      padding: 20px 0 70px 0;
+
+      label {
+        vertical-align: middle;
+      }
+
+      select {
+        width: 100px;
+        vertical-align: middle;
+      }
+    }
+
+    button {
+      width: 50px;
+      height: 25px;
+      cursor: pointer;
+    }
+  }
+</style>
